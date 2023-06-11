e915ace59f483cec36562d740655eb7d6d0e767e
secnoz <anshari.3101@gmail.com>
2023-06-12 00:09:55 +0800

    Membuat constructor dan setPenjumlahan

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -1,5 +1,17 @@
 package hitung;
 
 public class Matematika {
+    //variabel
+    private double bil1, bil2;
     
+    //constructor
+    public Matematika(double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    //method setPenjumlahan
+    public double setPenjumlahan(){
+        return bil1 + bil2;
+    }
 }
